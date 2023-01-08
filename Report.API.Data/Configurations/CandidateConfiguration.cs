e85a9f6f8efcb61690c188022e89d36b993016a9
Ahmet Bekman <ahmet.bekman@inventiv.com.tr>
2023-01-08 16:48:32 +0300

    Candidate seed data added and db migrated

--- Report.API.Data/Configurations/CandidateConfiguration.cs
+++ Report.API.Data/Configurations/CandidateConfiguration.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Report.API.Domain.Entities;
+using System;
 
 namespace Report.API.Data.Configurations
 {
@@ -9,6 +10,13 @@ namespace Report.API.Data.Configurations
         public void Configure(EntityTypeBuilder<Candidate> builder)
         {
             builder.Property(x => x.Name).HasMaxLength(250).IsRequired();
+
+            builder.HasData(
+                new Candidate { Id = Guid.NewGuid(), Name = "Candidate 1" },
+                new Candidate { Id = Guid.NewGuid(), Name = "Candidate 2" },
+                new Candidate { Id = Guid.NewGuid(), Name = "Candidate 3" },
+                new Candidate { Id = Guid.NewGuid(), Name = "Candidate 4" }
+            );
         }
     }
 }
---
+++ Report.API.Data/Migrations/20230108133900_Candidate.Seed.Data.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+
+namespace Report.API.Data.Migrations
+{
+    public partial class CandidateSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Candidate",
+                columns: new[] { "Id", "CreatedDate", "IsActive", "Name", "UpdatedDate" },
+                values: new object[,]
+                {
+                    { new Guid("3190d79f-b975-4f60-9f30-0b7e2c3a1eb6"), new DateTime(2023, 1, 8, 16, 39, 0, 510, DateTimeKind.Local).AddTicks(2933), true, "Candidate 1", null },
+                    { new Guid("d733adcf-45ad-4029-98b1-a4d269f39872"), new DateTime(2023, 1, 8, 16, 39, 0, 511, DateTimeKind.Local).AddTicks(904), true, "Candidate 2", null },
+                    { new Guid("5af7c85e-f941-4080-afc4-185852172d1b"), new DateTime(2023, 1, 8, 16, 39, 0, 511, DateTimeKind.Local).AddTicks(932), true, "Candidate 3", null },
+                    { new Guid("a31a3514-9414-49be-b361-47be256e831e"), new DateTime(2023, 1, 8, 16, 39, 0, 511, DateTimeKind.Local).AddTicks(934), true, "Candidate 4", null }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Candidate",
+                keyColumn: "Id",
+                keyValue: new Guid("3190d79f-b975-4f60-9f30-0b7e2c3a1eb6"));
+
+            migrationBuilder.DeleteData(
+                table: "Candidate",
+                keyColumn: "Id",
+                keyValue: new Guid("5af7c85e-f941-4080-afc4-185852172d1b"));
+
+            migrationBuilder.DeleteData(
+                table: "Candidate",
+                keyColumn: "Id",
+                keyValue: new Guid("a31a3514-9414-49be-b361-47be256e831e"));
+
+            migrationBuilder.DeleteData(
+                table: "Candidate",
+                keyColumn: "Id",
+                keyValue: new Guid("d733adcf-45ad-4029-98b1-a4d269f39872"));
+        }
+    }
+}
